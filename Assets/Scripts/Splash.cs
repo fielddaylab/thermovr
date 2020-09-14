b82a9d8638f2bd8da5f209ccf243429cc0ab6e4f
Phildo <phildo211@gmail.com>
2020-09-14 15:44:08 -0500

    splash screen

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+  public float minimumTimeToShowLogo = 5f;
+  public string levelToLoad = "MainScene";
+
+  public string introName = "SplashIn";
+  public string outroName = "SplashOut";
+
+  IEnumerator Start()
+  {
+    float minimumTimeEnd = Time.realtimeSinceStartup + minimumTimeToShowLogo;
+
+    // intro
+
+    // background load the new scene (but don't activate it yet)
+
+    AsyncOperation o = SceneManager.LoadSceneAsync(levelToLoad);
+    o.allowSceneActivation = true;
+    while(o.isDone)
+    {
+      yield return new WaitForEndOfFrame();
+    }
+
+    // delay until minimum time is reached
+
+    if (Time.realtimeSinceStartup < minimumTimeEnd)
+    {
+      yield return new WaitForSeconds(minimumTimeEnd - Time.realtimeSinceStartup);
+    }
+  }
+}
