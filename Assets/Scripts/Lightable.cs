16065737f86aab70a0ba2778eb23eaf7b569f4ae
levimhuillet <levimhuillet@gmail.com>
2023-04-18 15:06:40 -0500

    upgrade unity version to 2021.3.23f1 to fix second reload freeze

--- Assets/Scripts/Lightable.cs
+++ Assets/Scripts/Lightable.cs
@@ -7,46 +7,53 @@ using UnityEngine;
  * By default, just adds emission uniformly to the object, based on material/texture color.
  * Optionally, one can provide "lit" and "unlit" materials, instead of using a naive "emission" approach to lighting.
  **/
+[RequireComponent(typeof(MeshRenderer))]
 public class Lightable : MonoBehaviour
 {
-  public bool use_custom_mats;
-  public Material base_mat;
-  public Material lit_mat;
-  private MeshRenderer mesh_renderer;
+    public bool use_custom_mats;
+    public Material base_mat;
+    public Material lit_mat;
+    private MeshRenderer mesh_renderer;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    mesh_renderer = gameObject.GetComponent<MeshRenderer>();
-  }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh_renderer = gameObject.GetComponent<MeshRenderer>();
+    }
 
-  public void SetLit(bool on)
-  {
-    if (on)
+    public void SetLit(bool on)
     {
-      if (use_custom_mats)
-      {
-        mesh_renderer.material = lit_mat;
-      }
-      else
-      {
-        mesh_renderer.material.EnableKeyword("_Emission");
-        // if there is a texture, use grey light, which will be modified by texture to match color (it appears).
-        // else, emit light matching exactly to the material color (which wouldn't work so well if we had a black object, I suppose...)
-        Color color = (mesh_renderer.material.mainTexture != null) ? new Color(0.5f, 0.5f, 0.5f) : mesh_renderer.material.color;
-        mesh_renderer.material.SetColor("_EmissionColor", color);
-      }
+        if (mesh_renderer == null)
+        {
+            mesh_renderer = gameObject.GetComponent<MeshRenderer>();
+        }
+
+        if (on)
+        {
+            if (use_custom_mats)
+            {
+                mesh_renderer.materials[0] = lit_mat;
+            }
+            else
+            {
+                mesh_renderer.materials[0].EnableKeyword("_Emission");
+                // if there is a texture, use grey light, which will be modified by texture to match color (it appears).
+                // else, emit light matching exactly to the material color (which wouldn't work so well if we had a black object, I suppose...)
+                Color color = (mesh_renderer.materials[0].mainTexture != null) ? new Color(0.5f, 0.5f, 0.5f) : mesh_renderer.materials[0].color;
+                mesh_renderer.materials[0].SetColor("_EmissionColor", color);
+            }
+        }
+        else // Not On
+        {
+            if (use_custom_mats)
+            {
+                Debug.Log(this.gameObject.name);
+                mesh_renderer.materials[0] = base_mat;
+            }
+            else
+            {
+                mesh_renderer.materials[0].SetColor("_EmissionColor", Color.black);
+            }
+        }
     }
-    else // Not On
-    {
-      if (use_custom_mats)
-      {
-        mesh_renderer.material = base_mat;
-      }
-      else
-      {
-        mesh_renderer.material.SetColor("_EmissionColor", Color.black);
-      }
-    }
-  }
 }
