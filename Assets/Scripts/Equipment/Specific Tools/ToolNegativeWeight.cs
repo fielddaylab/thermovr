cdc13a3cf64254c1e856f8aed9524addd3f73fb6
levimhuillet <levimhuillet@gmail.com>
2023-11-08 17:03:24 -0600

    hydraulic press follows piston

--- Assets/Scripts/Equipment/Specific Tools/ToolNegativeWeight.cs
+++ Assets/Scripts/Equipment/Specific Tools/ToolNegativeWeight.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using ThermoVR.Physics;
 using UnityEngine;
 
 namespace ThermoVR.Tools
 {
     public class ToolNegativeWeight : Tool
     {
+        #region Inspector
+
+        [SerializeField] private FixedAnchor m_ElasticAnchor;
+
+        #endregion // Inspector
+
         #region Tool
 
         protected override IEnumerator ActivationRoutine() {
+            // disable anchor until activation completed
+            m_ElasticAnchor.enabled = false;
+
             gameObject.SetActive(true);
+
+            m_ElasticAnchor.enabled = true;
             yield return null;
         }
 
         protected override IEnumerator DeactivationRoutine() {
+            // disable anchor for deactivation
+            m_ElasticAnchor.enabled = false;
+
             gameObject.SetActive(false);
+            m_ElasticAnchor.enabled = true;
             yield return null;
         }
 
--- Assets/Scripts/Equipment/Specific Tools/ToolWeight.cs
+++ Assets/Scripts/Equipment/Specific Tools/ToolWeight.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using ThermoVR.Physics;
 using UnityEngine;
 
 namespace ThermoVR.Tools
 {
     public class ToolWeight : Tool
     {
+        #region Inspector
+
+        [SerializeField] private FixedAnchor m_HydraulicPressAnchor;
+
+        #endregion // Inspector
+
         #region Tool
 
         protected override IEnumerator ActivationRoutine() {
+            // disable anchor until activation completed
+            m_HydraulicPressAnchor.enabled = false;
+
             gameObject.SetActive(true);
+
+            m_HydraulicPressAnchor.enabled = true;
             yield return null;
         }
 
         protected override IEnumerator DeactivationRoutine() {
+            // disable anchor until deactivation completed
+            m_HydraulicPressAnchor.enabled = false;
+
             gameObject.SetActive(false);
+
+            m_HydraulicPressAnchor.enabled = true;
             yield return null;
         }
 
