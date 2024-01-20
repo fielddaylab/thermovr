b4bfd3653dd4a83643ed3b363bbe3ee2d9733d38
levimhuillet <levimhuillet@gmail.com>
2024-01-19 18:14:20 -0600

    improved spacing for longer instructions

--- Assets/Scripts/LabMode/InstructionLine.cs
+++ Assets/Scripts/LabMode/InstructionLine.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ThermoVR.Lab
 {
     public class InstructionLine : MonoBehaviour
     {
         public TMP_Text Text;
+        public RectTransform Rect;
+        public Graphic Img;
     }
 }
