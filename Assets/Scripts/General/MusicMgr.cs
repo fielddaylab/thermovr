f01af49cbbb239b509fdc999aed55a3c193409ea
levimhuillet <levimhuillet@gmail.com>
2024-08-21 16:05:41 -0500

    better music pausing

---
+++ Assets/Scripts/General/MusicMgr.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThermoVR
+{
+    public class MusicMgr : MonoBehaviour
+    {
+        [SerializeField] private AudioSource m_src;
+
+        private bool m_initialized = false;
+        private float m_pauseTime = 0;
+
+        private void OnEnable()
+        {
+            if (m_initialized)
+            {
+                m_src.time = m_pauseTime;
+                m_src.Play();
+            }
+            else
+            {
+                m_src.Play();
+                m_initialized = true;
+            }
+        }
+
+        private void Update()
+        {
+            m_pauseTime = m_src.time;
+        }
+
+        private void OnDisable()
+        {
+            m_src.Pause();
+        }
+    }
+
+}
