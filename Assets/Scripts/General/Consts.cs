81e13d5f778d35c605cc2a7e14106a2097422eb6
levimhuillet <levimhuillet@gmail.com>
2024-08-22 11:32:00 -0500

    Desktop start button

--- Assets/Scripts/General/Consts.cs
+++ Assets/Scripts/General/Consts.cs
@@ -5,7 +5,10 @@ using BeauUtil;
 
 static public class GameEvents
 {
-    static public readonly StringHash32 InitialLoadComplete = "world:initial-load-compelete";
+    static public readonly StringHash32 AsyncLoadComplete = "ui:async-load-complete";
+    static public readonly StringHash32 StartGameClicked = "ui:start-game-clicked";
+
+    static public readonly StringHash32 InitialLoadComplete = "world:initial-load-complete";
     static public readonly StringHash32 GatherPressables = "world:gather-pressables";
     static public readonly StringHash32 RegisterPressable = "world:register-pressable"; // Pressable
     static public readonly StringHash32 RegisterMovable = "world:register-movable"; // Touchable
